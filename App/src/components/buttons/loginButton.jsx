5f8dcaa759008f0969ce2200e568cac84cd5dc26
Luiz Felipe dos Santos Pereira <luizfsp.dev@gmail.com>
2023-09-17 20:24:21 -0300

    acertando button

--- App/src/components/buttons/loginButton.jsx
+++ App/src/components/buttons/loginButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-//import {FiLogIn} from 'react-icons/fi'
+import {FiLogIn} from 'react-icons/fi'
 const loginButton = () =>{
     return (
-        <a href="#">
-            Entrar
-
+        <a href="#" style={{color:"black",textDecoration: "none",borderRadius:"8px", padding:"4px", background: "linear-gradient(0deg, rgba(82,233,133,1) 0%, rgba(119,212,255,1) 100%)"}}>
+            Entrar<FiLogIn style={{marginLeft:"4px", color:"black"}}/>
         </a>
     )
 }
+
 
 export default loginButton
