86933237aaadbf5673f0824020abf45448720706
Luiz Felipe dos Santos Pereira <luizfsp.dev@gmail.com>
2023-09-24 19:53:42 -0300

    criando footer

--- App/src/components/Footer/index.jsx
+++ App/src/components/Footer/index.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
 import './index.css'
+import {BsGithub, BsLinkedin} from 'react-icons/Bs'
+import {FaUniversity} from 'react-icons/Fa'
 const Footer = () =>{
     return (
-        <div>
-            footer
-        </div>
+        <footer>
+            <did className="footer_website">
+                <div style={{width:"60px", height:"60px", backgroundColor:"green", textAlign:"center", borderRadius:"100%"}}>Logo</div>
+                <p>Imparcialmente te mostrando o que realmente importa</p>
+            </did>
+            <did className="footer_divider">
+                <h4>Institucional</h4>
+                <ul className="footer_list">
+                    <li>
+                        <a href="#">Sobre Nós</a>
+                    </li>
+                    <li>
+                        <a href="#">Política de Privacidade</a>
+                    </li>
+                </ul>
+            </did>
+            <did className="footer_divider">
+                <h4>O Projeto</h4>
+                <ul className="footer_list">
+                    <li>
+                        <a href="#">Funcionamento</a>
+                    </li>
+                    <li>
+                        <a href="#">Contribuição</a>
+                    </li>
+                    <li>
+                        <a href="#">Comunidade</a>
+                    </li>
+                </ul>
+            </did>
+            <did>
+                <h4>Nossas Redes</h4>
+                <ul className="footer_socials">
+                    <li>
+                        <a href="#"><BsGithub/></a>
+                    </li>
+                    <li>
+                        <a href="#"><BsLinkedin/></a>
+                    </li>
+                    <li>
+                        <a href="#"><FaUniversity/></a>
+                    </li>
+                </ul>
+            </did>
+        </footer>
     )
 }
 
