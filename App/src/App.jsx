6fc4f947a71f0add296489183b813e98016d44cc
aluno <aluno@UNIFEOB-ACAD.LOCAL>
2023-10-04 19:51:11 -0300

    limpando conteudo

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import Header from "./components/Header";
 import Stats from "./components/Stats";
 import Explanation from "./components/Explanation";
-import Reason from "./components/Reason";
 import Voting from "./components/Voting";
 import Register from "./components/Register";
 import Footer from "./components/Footer";
@@ -13,7 +12,6 @@ const App = () =>(
         <Hero/>
         <Stats/>
         <Explanation/>
-        <Reason/>
         <Voting/>
         <Register/>
         <Footer/>
--- App/src/components/Explanation/index.jsx
+++ App/src/components/Explanation/index.jsx
@@ -15,102 +15,93 @@ const Explanation = () =>{
             <ol>
                 <li>
                     <h4 style={{color:"#d00000"}}>Erradicação da Pobreza</h4>
-                    <p>Este objetivo visa acabar com a pobreza em todas as suas formas, garantindo que todas as pessoas tenham acesso a recursos essenciais, como alimentos, água potável, habitação, saúde e educação.</p>
+                    <p>Acabar com a pobreza em todas as suas formas, garantindo que todas as pessoas tenham acesso a recursos essenciais, como alimentos, água potável, habitação, saúde e educação.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#c9a227"}}>Fome Zero e Agricultura Sustentável</h4>
-                    <p>Busca assegurar a disponibilidade de alimentos suficientes, nutritivos e acessíveis para todos, ao mesmo tempo em que promove práticas agrícolas sustentáveis para proteger o meio ambiente.</p>
+                    <p>Assegurar a disponibilidade de alimentos suficientes, nutritivos e acessíveis para todos, ao mesmo tempo em que promove práticas agrícolas sustentáveis para proteger o meio ambiente.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#2b9348"}}>Saúde e Bem-Estar</h4>
-                    <p>Pretende garantir uma vida saudável e promover o bem-estar para todas as idades, melhorando o acesso a serviços de saúde, medicamentos e ações de prevenção.</p>
+                    <p>Garantir uma vida saudável e promover o bem-estar para todas as idades, melhorando o acesso a serviços de saúde, medicamentos e ações de prevenção.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#780000"}}>Educação de Qualidade</h4>
-                    <p>Busca garantir o acesso a uma educação inclusiva, equitativa e de qualidade, promovendo oportunidades de aprendizado ao longo da vida.</p>
+                    <p>Garantir o acesso a uma educação inclusiva, equitativa e de qualidade, promovendo oportunidades de aprendizado ao longo da vida.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#D93B29"}}>Igualdade de Gênero</h4>
-                    <p>Visa alcançar a igualdade de gênero e empoderar todas as mulheres e meninas, eliminando a discriminação de gênero e promovendo a participação igualitária em todas as áreas.</p>
+                    <p>Alcançar a igualdade de gênero e empoderar todas as mulheres e meninas, eliminando a discriminação de gênero e promovendo a participação igualitária em todas as áreas.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#1DCDD0"}}>Água Limpa e Saneamento</h4>
-                    <p>Tem como objetivo garantir a disponibilidade e gestão sustentável da água e saneamento para todos, reduzindo a escassez de água e melhorando a qualidade da água. </p>
+                    <p>Garantir a disponibilidade e gestão sustentável da água e saneamento para todos, reduzindo a escassez de água e melhorando a qualidade da água. </p>
                 </li>
                 <li>
                     <h4 style={{color:"#997D10"}}>Energia Limpa e Acessível</h4>
-                    <p>Busca assegurar o acesso universal a fontes de energia confiáveis, sustentáveis e modernas, ao mesmo tempo em que promove a eficiência energética e o uso de energias renováveis.</p>
+                    <p>Assegurar o acesso universal a fontes de energia confiáveis, sustentáveis e modernas, ao mesmo tempo em que promove a eficiência energética e o uso de energias renováveis.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#722525"}}>Trabalho Decente e Crescimento Econômico</h4>
-                    <p> Visa promover o crescimento econômico sustentável, inclusivo e sustentável, garantindo empregos decentes para todos e erradicando o trabalho infantil e o trabalho escravo.</p>
+                    <p>Promover o crescimento econômico sustentável, inclusivo e sustentável, garantindo empregos decentes para todos e erradicando o trabalho infantil e o trabalho escravo.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#D94D11"}}>Indústria, Inovação e Infraestrutura</h4>
-                    <p>Tem como objetivo construir infraestrutura resiliente, promover a industrialização inclusiva e sustentável, e fomentar a inovação e a adoção de tecnologias limpas.</p>
+                    <p>Construir infraestrutura resiliente, promover a industrialização inclusiva e sustentável, e fomentar a inovação e a adoção de tecnologias limpas.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#861875"}}>Redução das Desigualdades</h4>
-                    <p>Busca reduzir a desigualdade dentro e entre países, promovendo políticas que beneficiem os mais vulneráveis e abordando questões como a desigualdade de renda.</p>
+                    <p>Reduzir a desigualdade dentro e entre países, promovendo políticas que beneficiem os mais vulneráveis e abordando questões como a desigualdade de renda.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#B46F25"}}>Cidades e Comunidades Sustentáveis</h4>
-                    <p>Visa tornar as cidades e os assentamentos humanos mais inclusivos, seguros, resilientes e sustentáveis, promovendo o planejamento urbano adequado e o uso eficiente dos recursos.</p>
+                    <p>Tornar as cidades e os assentamentos humanos mais inclusivos, seguros, resilientes e sustentáveis, promovendo o planejamento urbano adequado e o uso eficiente dos recursos.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#8B5E2F"}}>Consumo e Produção Responsáveis</h4>
-                    <p>Pretende promover padrões de consumo e produção sustentáveis, incluindo o uso eficiente de recursos naturais, a redução do desperdício e a conscientização sobre os impactos ambientais.</p>
+                    <p>Promover padrões de consumo e produção sustentáveis, incluindo o uso eficiente de recursos naturais, a redução do desperdício e a conscientização sobre os impactos ambientais.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#2F613C"}}>Ação contra a Mudança Global do Clima</h4>
-                    <p>Busca tomar medidas urgentes para combater as mudanças climáticas e seus impactos, incluindo a redução das emissões de gases de efeito estufa e a adaptação às mudanças climáticas.</p>
+                    <p>Tomar medidas urgentes para combater as mudanças climáticas e seus impactos, incluindo a redução das emissões de gases de efeito estufa e a adaptação às mudanças climáticas.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#225B9C"}}>Vida na Água</h4>
-                    <p>Visa conservar e usar de forma sustentável os oceanos, mares e recursos marinhos para o desenvolvimento sustentável, incluindo a proteção da vida marinha.</p>
+                    <p>Conservar e usar de forma sustentável os oceanos, mares e recursos marinhos para o desenvolvimento sustentável, incluindo a proteção da vida marinha.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#2B9C4D"}}>Vida Terrestre</h4>
-                    <p>Tem como objetivo proteger, restaurar e promover o uso sustentável dos ecossistemas terrestres, gerenciar de forma sustentável florestas, combater a desertificação e deter a perda de biodiversidade.</p>
+                    <p>Proteger, restaurar e promover o uso sustentável dos ecossistemas terrestres, gerenciar de forma sustentável florestas, combater a desertificação e deter a perda de biodiversidade.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#183993"}}>Paz, Justiça e Instituições Eficazes</h4>
-                    <p>Busca promover sociedades pacíficas, justas e inclusivas, fortalecendo as instituições para a promoção do Estado de Direito e o acesso à justiça.</p>
+                    <p>Promover sociedades pacíficas, justas e inclusivas, fortalecendo as instituições para a promoção do Estado de Direito e o acesso à justiça.</p>
                 </li>
                 <li>
                     <h4 style={{color:"#0739AF"}}>Parcerias e Meios de Implementação</h4>
-                    <p>Pretende fortalecer a implementação dos ODS por meio de parcerias globais e recursos financeiros, tecnológicos e de desenvolvimento humano.</p>
+                    <p>Fortalecer a implementação dos ODS por meio de parcerias globais e recursos financeiros, tecnológicos e de desenvolvimento humano.</p>
                 </li>
             </ol>
             <p>
                 Esses objetivos servem como um roteiro global para orientar os esforços dos governos, organizações não governamentais, empresas e a sociedade civil na promoção do desenvolvimento sustentável em todo o mundo. Cada país é incentivado a adaptar os ODS às suas próprias realidades e necessidades, criando planos nacionais para alcançar essas metas até 2030.
             </p>
         </div>
-        <div>
-            <h2>Pq esse site existe?</h2>
+        <div className='reason'>
+            <h2>Nosso Propósito</h2>
             <ol>
                 <li>
-                    <h4>Conscientização e Educação:</h4>
+                    <h4>Conscientização e Educação</h4>
                     <p>
-                        Um website dedicado aos ODS pode servir como uma plataforma educativa, informando o público sobre a importância dessas metas globais e como elas se relacionam com a vida cotidiana no Brasil. Isso ajuda a aumentar a conscientização e a compreensão da sociedade sobre questões críticas de desenvolvimento sustentável.
+                        Servimos como uma plataforma educativa, informando o público sobre a importância dessas metas globais e como elas se relacionam com a vida cotidiana no Brasil. Isso ajuda a aumentar a conscientização e a compreensão da sociedade sobre questões críticas de desenvolvimento sustentável.
                     </p>
                 </li>
                 <li>
-                    <h4>Engajamento da Sociedade Civil:</h4>
-                    <p>
-                        O website pode servir como um hub para a sociedade civil, ONGs, acadêmicos e outros grupos interessados em colaborar e fazer lobby em questões relacionadas aos ODS. Facilita a troca de informações e o engajamento da comunidade em iniciativas de desenvolvimento sustentável.
-                    </p>
-                </li>
-                <li>
-                    <h4>Incentivo à Inovação:</h4>
+                    <h4>Incentivo à Inovação</h4>
                     <p>
                         À medida que os dados são disponibilizados e o progresso é acompanhado, isso pode inspirar a inovação em soluções e abordagens para questões de desenvolvimento sustentável. Empresas e empreendedores podem encontrar oportunidades para criar negócios socialmente responsáveis.
                     </p>
                 </li>
             </ol>
-            <p>
-                Em resumo, criar um website para acompanhar dados relacionados aos ODS no Brasil é uma maneira eficaz de promover o progresso em direção a um desenvolvimento sustentável, envolvendo a sociedade, acompanhando o progresso e influenciando as políticas e práticas que moldam o futuro do país.
-            </p>
         </div>
     </section>
     )
