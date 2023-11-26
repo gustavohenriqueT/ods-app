f87647e5293f87338961efc52f36e506b7bf0b3e
Luiz Felipe dos Santos Pereira <luizfsp.dev@gmail.com>
2023-11-26 19:28:45 -0300

    codigo para producao

---
+++ Api/index.js
@@ -0,0 +1,21 @@
+// (async () => {
+//   const db = require("./db");
+
+//   // SELECT
+//   const teste = await db.selectCustomers();
+//   console.log(teste);
+
+//   // INSERT
+//   console.log ('INSERT INTO teste');
+//   const insert = await db.insertCustomers({idteste: 3, testecol: "foi"});
+//   console.log(insert);
+
+//   // UPDATE
+//   const update = await db.updateCustomers(3, {testecol:"arrumando"});
+//   console.log(update);
+
+//   // DELETE
+//   const delet = await db.deleteCustomers(3);
+//   console.log(delet);
+
+// })();
--- App/vite.config.js
+++ App/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import ReactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000, // Especifique a porta desejada aqui
   },
-  plugins: [react()],
+  plugins: [react(),ReactRefresh()],
 });
